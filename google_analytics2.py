ac1909bec2591c02c67c7c4e10ce3da15d7f399f
Lasse Pekkarinen <lvp@iki.fi>
2017-05-10 20:33:18 +0300

    Update google_analytics2.py

--- google_analytics2.py
+++ google_analytics2.py
@@ -17,7 +17,7 @@ logger1 = logging.getLogger('google_analytics')
 
 
 def send_ga(request):
-    PROPERTY_ID = "***REMOVED***"
+    PROPERTY_ID = "UA-XXXXXXXX"
     data = {
         'v': '1',  # API Version.
         'tid': PROPERTY_ID,  # Tracking ID / Property ID.
